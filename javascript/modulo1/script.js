4bb9d1e9898ee33bf587e5837616124149f52ce8
Tiago Rodrigues de Souza - 3898 <tiagor@sescdf.com.br>
2021-10-13 15:45:01 -0300

    Módulo 1 Finalizado e iniciado Módulo 2 até a aula 3

--- javascript/modulo1/script.js
+++ javascript/modulo1/script.js
@@ -1,1 +1,62 @@
-console.log("Imprimindo no log...");
+// // Aula 4 Formas de mostrar dados na tela
+// // 1 - manipulando tags html pelo ID */
+// document.getElementById("titulo").innerHTML = "Novo título para página";
+// // 2 - Escrevendo direto na página
+// document.write("Texto com documento write");
+// // 3 - Utilizando a janela do navegador
+// window.alert("Mensagem de exemplo com window alert");// ou alert("Mensagem de exemplo com window alert");
+// // 4 - Utilizando o console
+// console.log("Imprimindo no log...");
+
+
+// // Aula 5 Variáveis
+// var nome = "Tiago Rodrigues";
+// var idade = 32;
+// console.log(nome + ', possui ' + idade + ' anos.');
+
+// var x = 10;
+// var y = 15;
+// var total = x+y;
+// console.log(total);
+
+// var x = "10";
+// var y = "15";
+// var total = x+y;
+// console.log(total);
+
+
+// Aula 6 e 7 Condicionais 
+// var hora = 20;
+
+// if (hora < 12) {
+//     console.log("Bom dia");
+// } else if ( hora < 18) {
+//     console.log("Boa tarde");
+// } else if (hora <= 23) {
+//     console.log("Boa noite");
+// }
+
+// if((hora >= 8 && hora <= 9)||(hora >= 17 && hora <= 18)){
+//     console.log("Você está na hora do rush!");
+// }
+
+// Aula 8 Variáveis
+
+var nome1 = 'Tiago';//Espoco geral (HOISTING)
+if(nome1=='teste'){
+    var idadeTiago = 32
+}
+console.log(idadeTiago);
+
+let nome2 = 'Vanessa';//Escopo onde foi definido
+if(nome2=='teste'){
+    let idadeVanessa = 32
+}
+//console.log(idadeVanessa);//Uncaught ReferenceError: idadeVanessa is not defined
+nome2 = 'outro nome';
+//let nome2 = 'Vanessa2';//Não é possível redeclarar variáveis com let -> Uncaught SyntaxError: redeclaration of let nome2 
+
+const nome3 = 'Christian';//Variáveis com valor constante
+
+const pessoa = {nome: 'Christian', idade:3, peso: 17.5};//Variáveis com valor constante
+console.log(pessoa);
---
+++ javascript/modulo2/script.js
@@ -0,0 +1,9 @@
+//DOM = Documento Object Model
+
+//Selecionando Elemento pelo ID Aula 2
+document.getElementById("exemplo").innerHTML="Christian Lucena";
+
+//Selecionando Coleção de Elementos Aula 3
+document.getElementsByClassName("lista")[0].innerHTML="Item alterado!";
+document.getElementsByTagName("button")[0].innerHTML='Botão Alterado';
+document.getElementsByName("e-mail")[0].value='teste@teste.com';
